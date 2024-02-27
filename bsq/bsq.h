c7b93d3ba6d1c15002342aec8001453750e926ce
Anna Semsey <asemsey@1-C-10.42heilbronn.de>
2024-02-27 15:21:00 +0100

    added some solver funcs

--- bsq/bsq.h
+++ bsq/bsq.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bsq.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fnikzad <fnikzad@student.42.fr>            +#+  +:+       +#+        */
+/*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:16:57 by asemsey           #+#    #+#             */
-/*   Updated: 2024/02/27 14:01:03 by fnikzad          ###   ########.fr       */
+/*   Updated: 2024/02/27 14:30:41 by asemsey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,22 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
 typedef struct s_bsq
 {
-    char	**map;
-    int		height;
+	char	**map;
+	int		height;
 	int		width;
+	t_point	size;
 	char	full;
 	char	empty;
 	char	obs;
-}   t_bsq;
+}	t_bsq;
 
 size_t		ft_strlen(const char *s);
 char		*ft_strjoin(char *s1, const char *s2);
--- bsq/solve.c
+++ bsq/solve.c
@@ -6,7 +6,7 @@
 /*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:17:39 by asemsey           #+#    #+#             */
-/*   Updated: 2023/11/03 15:01:08 by asemsey          ###   ########.fr       */
+/*   Updated: 2024/02/27 15:20:38 by asemsey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,79 @@
 
 // input: a valid map string array (1 str per line) -> use ft_split!
 // the first line is "[lines][empty][obstacle][full]\n"
+
+t_point	solve(t_bsq *bsq)
+{
+	return (bsq->size);
+	// incomplete
+}
+
+// check if there are obstacles in the given square
+int	is_square(t_bsq *bsq, t_point start, int size)
+{
+	int	y;
+	int	x;
+
+	if (!bsq->map[start.y] || !bsq->map[start.y][start.x]
+		|| start.y + (size - 1) >= bsq->height
+		|| start.x + (size - 1) >= bsq->width)
+		return (0);
+	y = 0;
+	while (bsq->map[start.y + y] && start.y + y < bsq->height)
+	{
+		x = 0;
+		while (bsq->map[start.y + y][start.x + x] && start.x + x < bsq->width)
+			if (bsq->map[start.y + y][start.x + x] != bsq->empty)
+				return (0);
+		y++;
+	}
+	return (1);
+}
+
+// get the size of the biggest square starting at start
+int	get_square(t_bsq *bsq, t_point start)
+{
+	int		i;
+	int		max_size;
+
+	if (!bsq->map[start.y] || !bsq->map[start.y][start.x])
+		return (0);
+	i = 1;
+	if (bsq->height < bsq->width)
+		max_size = bsq->height;
+	else
+		max_size = bsq->width;
+	while (i <= max_size)
+	{
+		if (!is_square(bsq, start, i))
+			break ;
+		i++;
+	}
+	return (i - 1);
+}
+
+// fill the solution square in the map
+void	solution(t_bsq *bsq, t_point start)
+{
+	int		size;
+	int		x;
+	int		y;
+	char	**map;
+
+	size = get_square(bsq, start);
+	if (size <= 0)
+		return ;
+	map = bsq->map;
+	y = 0;
+	while (map[start.y + y] && start.y + y < bsq->height && y < size)
+	{
+		x = 0;
+		while (map[start.y + y][start.x + x] && start.x + x < bsq->width
+			&& x < size)
+			map[start.y + y][start.x + x++] = bsq->full;
+		y++;
+	}
+}
 
 // solver:
 // marks biggest gap in each line [full]
