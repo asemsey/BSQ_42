4fd8502613d1cc5d503b72029c3918c5957776d6
Fayeza Nikzad <fnikzad@1-C-12.42heilbronn.de>
2024-02-27 14:25:45 +0100

    addded

--- bsq/bsq.h
+++ bsq/bsq.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bsq.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fnikzad <fnikzad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:16:57 by asemsey           #+#    #+#             */
-/*   Updated: 2023/11/03 15:04:26 by asemsey          ###   ########.fr       */
+/*   Updated: 2024/02/27 14:01:03 by fnikzad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,18 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char *s1, const char *s2);
-char	**ft_split(char const *s, char c);
+typedef struct s_bsq
+{
+    char	**map;
+    int		height;
+	int		width;
+	char	full;
+	char	empty;
+	char	obs;
+}   t_bsq;
+
+size_t		ft_strlen(const char *s);
+char		*ft_strjoin(char *s1, const char *s2);
+char		**ft_split(char const *s, char c);
 
 #endif
--- bsq/main.c
+++ bsq/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fnikzad <fnikzad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:08:41 by asemsey           #+#    #+#             */
-/*   Updated: 2023/11/03 15:01:04 by asemsey          ###   ########.fr       */
+/*   Updated: 2024/02/27 14:25:14 by fnikzad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,23 @@
 
 // int	
 
+// void	read_map(t_bsq *map, char *s)
+// {
+// 	int i = 0;
+// 	int fd;
+// 	fd = open(s, O_RDONLY);
+// 	if (fd < 0)
+// 	{
+// 		write
+// 	}
+// }
+
 int	main(int argc, char const *argv[])
 {
 	int		file;
 	char	*map;
-
+	t_bsq	game;
+	
 	file = 1;
 	if (argc < 2)
 		// ???
