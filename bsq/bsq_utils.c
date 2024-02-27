d6d9fe15f8d6c16e8f835a0a61c767cd722bc815
Fayeza Nikzad <fnikzad@1-C-12.42heilbronn.de>
2024-02-27 15:19:35 +0100

    added

--- bsq/bsq_utils.c
+++ bsq/bsq_utils.c
@@ -3,48 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   bsq_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fnikzad <fnikzad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:16:03 by asemsey           #+#    #+#             */
-/*   Updated: 2023/11/03 15:04:32 by asemsey          ###   ########.fr       */
+/*   Updated: 2024/02/27 14:56:53 by fnikzad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bsq.h"
 
-size_t	ft_strlen(const char *s)
-{
-	int	i;
 
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
+// char	*ft_strjoin(char *s1, const char *s2)
+// {
+// 	char	*s;
+// 	size_t	i;
+// 	size_t	j;
 
-char	*ft_strjoin(char *s1, const char *s2)
+// 	i = 0;
+// 	s = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+// 	if (!s || (!s1 && !s2))
+// 		return (NULL);
+// 	while (s1 && s1[i] != '\0')
+// 	{
+// 		s[i] = s1[i];
+// 		i++;
+// 	}
+// 	j = 0;
+// 	while (s2 && s2[j] != '\0')
+// 	{
+// 		s[i + j] = s2[j];
+// 		j++;
+// 	}
+// 	s[i + j] = '\0';
+// 	if (s1)
+// 		free(s1);
+// 	return (s);
+// }
+
+
+char	*ft_strdup(const char *s1)
 {
-	char	*s;
+	char	*dup;
+	size_t	l;
 	size_t	i;
-	size_t	j;
 
+	l = ft_strlen(s1);
 	i = 0;
-	s = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!s || (!s1 && !s2))
-		return (NULL);
-	while (s1 && s1[i] != '\0')
+	dup = (char *) malloc (sizeof(char) * (l + 1));
+	if (!dup)
+		return (0);
+	while (s1[i] != 0)
 	{
-		s[i] = s1[i];
+		dup[i] = s1[i];
 		i++;
 	}
-	j = 0;
-	while (s2 && s2[j] != '\0')
-	{
-		s[i + j] = s2[j];
-		j++;
-	}
-	s[i + j] = '\0';
-	if (s1)
-		free(s1);
-	return (s);
+	dup[i] = 0;
+	return (dup);
 }
--- bsq/main.c
+++ bsq/main.c
@@ -6,7 +6,7 @@
 /*   By: fnikzad <fnikzad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:08:41 by asemsey           #+#    #+#             */
-/*   Updated: 2024/02/27 14:25:14 by fnikzad          ###   ########.fr       */
+/*   Updated: 2024/02/27 15:19:16 by fnikzad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,35 +17,103 @@
 
 // int	
 
-// void	read_map(t_bsq *map, char *s)
-// {
-// 	int i = 0;
-// 	int fd;
-// 	fd = open(s, O_RDONLY);
-// 	if (fd < 0)
-// 	{
-// 		write
-// 	}
-// }
-
-int	main(int argc, char const *argv[])
+int ft_new_strlen(char *s)
 {
-	int		file;
-	char	*map;
+	int i = 0;
+	while (s && s[i] != '\0' && s[i] != '\n')
+		i++;
+	return (i);
+}
+
+void	get_height(t_bsq *game, char *s)
+{
+	int i = 0;
+	int fd;
+	char *line;
+	
+	fd = open (s, O_RDONLY);
+	if (fd < 0)
+	{
+		write (2, "Error\n", 1);
+		exit (1);
+	}
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		free (line);
+		i++;
+	}
+	game->height = i;
+}
+
+void	read_map(t_bsq *game, char *s)
+{
+	int i = 0;
+	char *line;
+	int fd;
+	fd = open(s, O_RDONLY);
+	if (fd < 0)
+	{
+		write (2, "Error\n", 1);
+		exit (1);
+	}
+	get_height(game, s);
+	game->map = malloc (sizeof(char *) * (game->height + 1));
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		game->map[i] = ft_strdup(line);
+		free(line);
+		i++;
+	}
+	game->map[i] = NULL;
+}
+
+void	ft_ptint_map(char **game)
+{
+	int i = 0;
+	int j;
+	while (game[i])
+	{
+		j = 0;
+		while(game[i][j])
+		{
+			write (1, &game[i][j], 1);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	// int		file;
+	int i = 1;
+	// char	*map;
 	t_bsq	game;
 	
-	file = 1;
+	while (argv[i])
+	{
+		read_map(&game, argv[i]);
+		i++;
+	}
+	i = 0;
+	
+	ft_ptint_map(game.map);
+
+
+	
+	// file = 1;
 	if (argc < 2)
 		// ???
-	while (argv[file])
-	{
-		// open file
-		// read it to map
-		// check valid map
-		// solve(map)
-		// close file
-		// free map
-		file++;
-	}
+
+	// while (argv[file])
+	// {
+	// 	// open file
+	// 	// read it to map
+	// 	// check valid map
+	// 	// solve(map)
+	// 	// close file
+	// 	// free map
+	// 	file++;
+	// }
 	return (0);
 }
