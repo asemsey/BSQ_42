fabff5c6f3287516c536708283a0d32b10acd447
Anna Semsey <asemsey@1-C-12.42heilbronn.de>
2024-02-28 10:50:51 +0100

    solver fixed

--- bsq/solve.c
+++ bsq/solve.c
@@ -6,7 +6,7 @@
 /*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 14:17:39 by asemsey           #+#    #+#             */
-/*   Updated: 2024/02/27 17:11:06 by asemsey          ###   ########.fr       */
+/*   Updated: 2024/02/28 10:50:23 by asemsey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,11 +60,11 @@ int	is_square(t_bsq *bsq, t_point start, int size)
 	int	x;
 
 	if (start.y >= bsq->height || start.x >= bsq->width
-		|| start.y + (size - 1) >= bsq->height
+		|| start.y + (size - 1) >= bsq->height || bsq->real_map[start.y][start.x] == bsq->obs
 		|| start.x + (size - 1) >= bsq->width)
 		return (0);
 	y = 0;
-	while (start.y + y >= bsq->height && y < size)
+	while (start.y + y < bsq->height && y < size)
 	{
 		x = 0;
 		while (start.x + x < bsq->width && x < size)
@@ -84,7 +84,7 @@ int	get_square(t_bsq *bsq, t_point start)
 	int		i;
 	int		max_size;
 
-	if (!bsq->real_map[start.y] || !bsq->real_map[start.y][start.x])
+	if (start.y >= bsq->height || start.x >= bsq->width)
 		return (0);
 	i = 1;
 	if (bsq->height < bsq->width)
@@ -93,7 +93,7 @@ int	get_square(t_bsq *bsq, t_point start)
 		max_size = bsq->width;
 	while (i <= max_size)
 	{
-		if (!is_square(bsq, start, i))
+		if (is_square(bsq, start, i) == 0)
 			break ;
 		i++;
 	}
